6969f3a608bae21c02eca3b3d65f265151edc8d0
clivezhu <clivezhu@tencent.com>
2023-08-14 17:10:24 +0800

    fix: 删除 token

--- settings/settings.py
+++ settings/settings.py
@@ -1,5 +1,5 @@
-NOTION_TOKEN = 'secret_D7n23eqjBGn7qEBK1d9zzU12ZBHpvHh4dyNyoHmnZYm'
-WEREAD_COOKIE = 'pgv_info=ssid=s7502725268; pgv_pvid=164828675; authkey=7008001003186022607ad8bcb3f18b4e0d2b39f7416e1d4d27c5ad2f2b73db48bbd0424c9ff62f5b16fc4c1a4e9c048716076f7f969b8fdaec7c27fd76e0c5454d2a4197ab56ebfad3886eb33ab7cfd61210f01896c6dd5131726b4f97f9d6fc3d902473f434dd9b7c&weixinnum=1688855420493742; uin=null; skey=null; luin=null; lskey=null; user_id=null; session_id=null; wr_fp=1419937758; wr_gid=200236901; wr_vid=16752020; wr_skey=7x0Cq_99; wr_pf=0; wr_rt=web%40JLA7hxozrFOUg4PmG6O_AL; wr_localvid=7dd325c06ff9d947ddbecb2; wr_name=%E6%9F%8F%E5%B0%94; wr_avatar=https%3A%2F%2Fwx.qlogo.cn%2Fmmhead%2FhIz3ylFIYSibvbDFVkrIktgeW9EnicBfp9FLC5mDOMcp1nibR6T2DicE1w%2F0; wr_gender=1'
-DATABASE_ID = '406a4180398a426e82f4bd45d36f8b24'
+NOTION_TOKEN = ' '
+WEREAD_COOKIE = ' '
+DATABASE_ID = ' '
 
 BOOK_BLACKLIST = []
