44930023f133ff1e9161f45325c581b57fbd7d98
clivezhu <clivezhu@tencent.com>
2023-08-14 16:54:41 +0800

    feat: 增加 token

--- settings/settings.py
+++ settings/settings.py
@@ -1,22 +1,5 @@
-NOTION_TOKEN = 'xxxxxxxxxxx'
-WEREAD_COOKIE = 'wr_fp=2533447432; wr_gid=295032143; wr_vid=23683664; wr_pf=0; wr_rt=web%40QxPpLky5s9q8MDFU8TK_AL; wr_localvid=fb3325d071696250fb37ae2; wr_name=%E8%AF%B6%E9%B8%AD; wr_avatar=https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FXpfWjDr8uAYiagxaSibFmnMh2LUJXTQrjJ8z2ve6X8J3w2CI79YSvQwic1icyHUoHVgoccckuoLxlt1cQOD87tZLfQ%2F132; wr_gender=1; RK=Ou2xAkMpZ9; ptcz=6d556d39aaf44f088fec74b97d2942db2ea837b96f5c8c603ddacec2b5e1fd45; uin=o1307317886; wr_skey=t1Gw1zJp'
-DATABASE_ID = 'd92bb4b8434745baa2061caf67d6ef7a'
+NOTION_TOKEN = 'secret_D7n23eqjBGn7qEBK1d9zzU12ZBHpvHh4dyNyoHmnZYm'
+WEREAD_COOKIE = 'pgv_info=ssid=s7502725268; pgv_pvid=164828675; authkey=7008001003186022607ad8bcb3f18b4e0d2b39f7416e1d4d27c5ad2f2b73db48bbd0424c9ff62f5b16fc4c1a4e9c048716076f7f969b8fdaec7c27fd76e0c5454d2a4197ab56ebfad3886eb33ab7cfd61210f01896c6dd5131726b4f97f9d6fc3d902473f434dd9b7c&weixinnum=1688855420493742; uin=null; skey=null; luin=null; lskey=null; user_id=null; session_id=null; wr_fp=1419937758; wr_gid=200236901; wr_vid=16752020; wr_skey=7x0Cq_99; wr_pf=0; wr_rt=web%40JLA7hxozrFOUg4PmG6O_AL; wr_localvid=7dd325c06ff9d947ddbecb2; wr_name=%E6%9F%8F%E5%B0%94; wr_avatar=https%3A%2F%2Fwx.qlogo.cn%2Fmmhead%2FhIz3ylFIYSibvbDFVkrIktgeW9EnicBfp9FLC5mDOMcp1nibR6T2DicE1w%2F0; wr_gender=1'
+DATABASE_ID = '406a4180398a426e82f4bd45d36f8b24'
 
-BOOK_BLACKLIST = [
-    '从红月开始',
-    '修真聊天群（聊天群的日常生活）',
-    '第一序列',
-    '我真没想重生啊',
-    '我有一座恐怖屋',
-    '大王饶命',
-    '我师兄实在太稳健了',
-    '死而替生',
-    '中医许阳',
-    '这游戏也太真实了',
-    '家父汉高祖',
-    '吕布的人生模拟器',
-    '学霸的黑科技系统',
-    '黎明之剑',
-    '我的一天有48小时',
-    '明克街13号',
-]
+BOOK_BLACKLIST = []
